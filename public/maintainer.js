9f0f53d2a912c028e5dbbc5f036f39276a9a317b
skkmania <skkmania@ubu-pg84>
2010-11-08 23:12:30 +0900

    pseudo-waveサーバ用メンテナクラスを追加することにした

---
+++ public/maintainer.js
@@ -0,0 +1,11 @@
+// maintainer.js
+// 2010/11/8
+// pseudo-wave.rbを使うアプリケーションをテストするときに
+// サーバ側が持っているstateを書き換えるなどのメンテナンス作業を
+// するためのwindowを表示する
+
+var Maintainer = Class.create({
+  initialize : function initialize(controller) {
+    this.
+  },
+});
