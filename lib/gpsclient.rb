7660894ff03a97740d0c29b879099e10907df6ed
skkmania on sq-gps <skkmania@localnet>
2010-12-15 12:39:50 +0900

    gpsshogiのバイナリを動かし通信できるこを確認するテストを追加した。(refs #180)

--- lib/gpsclient.rb
+++ lib/gpsclient.rb
@@ -2,14 +2,28 @@ require 'rb_gpsshogi.rb'
 require 'lib/store.rb'
 
 class GpsClient < GpsShogi
-  def initialize
+  def initialize(config)
     @status = nil
     @store  = Store.new
+    super config
+    #@gps    = GpsShogi.new(config)
   end
   attr_accessor :status, :store
     
   def send(move)
-    # $super(move)
+    super(move)
     @status = 'sent'
+  end
+
+  def read
+    @status = 'read'
+    super
+  end
+
+  def close
+  end
+
+  def toryo
+    send "%TORYO"
   end
 end
--- spec/lib/gpsclient_spec.rb
+++ spec/lib/gpsclient_spec.rb
@@ -1,24 +1,62 @@
 require 'gpsclient.rb'
 
+$gps_config = { :initial_filename => "bin/csa.init",
+             :opponent => "skkmania",
+             :sente => false,
+             :black => "skkmania", 
+             :white => "gps",
+             :limit => 1600, 
+             :table_size => 30000,
+             :table_record_limit => 50,
+             :node_limit => 16000000,
+             :timeleft => 100, 
+             :byoyomi => 60,
+             :logfile_basename => "bin/logs/x1_",
+             :other_options => "",
+             :base_command => 'bin/gpsshogi -v -c'
+           }
+
 describe GpsClient, "は初期化されたとき" do
-  before do
-    @gpsclient = GpsClient.new
+  before(:all) do
+    @gpsclient = GpsClient.new($gps_config)
+  end
+
+  after(:all) do
+    @gpsclient.toryo
+    sleep 1
+    puts 'toryo done.'
   end
 
   it "のstatus はnilである" do
     @gpsclient.status.should be_nil
   end
+
   it "のstore プロパティをもつ" do
     @gpsclient.store.should_not be_nil
   end
 end
 
-describe GpsClient, "は#sendしたとき" do
-  before do
-    @gpsclient = GpsClient.new
-    @gpsclient.send('+7776FU')
+describe GpsClient, "は#sendしたあと" do
+  before(:all) do
+    @gpsclient = GpsClient.new($gps_config)
+    sleep 1
+    @gpsclient.send "+7776FU\n"
+    sleep 1
   end
+
+  after(:all) do
+    @gpsclient.toryo
+    sleep 1
+    puts 'toryo done.'
+  end
+
   it "のstatus はsentである" do
     @gpsclient.status.should == 'sent'
+  end
+
+  it "gpsclientからのレスポンスを読むことができる" do
+    res = @gpsclient.read
+    puts res
+    res.should_not be_nil
   end
 end
